c37aa2cfcbc35b09c63f83c98cdf683eb5f0ae19
lorgik <sib.lorgik@gmail.com>
2024-08-28 23:50:21 +0300

    added dynamic sign

--- app/operations/expenses/page.tsx
+++ app/operations/expenses/page.tsx
@@ -11,6 +11,7 @@ import { formatNumber, formatNumberWithSign } from '@/utils/formatting'
 
 function Expenses() {
   const [conicGradient, setConicGradient] = useState('')
+  const [bounded, setBounded] = useState(true)
 
   const operations = useSelector((state: RootState) => state.operations)
   const categories = useSelector((state: RootState) => state.categories)
@@ -128,7 +129,7 @@ function Expenses() {
       </div>
 
       <div className={styles.signs}>
-        <div className={`${styles.sign} ${styles.expenses}`}>
+        <div className={`${styles.sign} ${styles.expenses} ${bounded && styles.bounded}`}>
           <h5 className={styles.name}>Ваши траты</h5>
           <div
             className={styles.pie}
@@ -163,15 +164,43 @@ function Expenses() {
               </div>
             ))}
           </div>
+          <div className={styles.shape} onClick={() => setBounded(false)}>
+            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M16.6192 7.84171C16.1116 7.3861 15.2884 7.3861 14.7808 7.84171L10.5 11.6834L6.21924 7.84171C5.71156 7.3861 4.88844 7.3861 4.38076 7.84171C3.87308 8.29732 3.87308 9.03601 4.38076 9.49163L9.58076 14.1583C10.0884 14.6139 10.9116 14.6139 11.4192 14.1583L16.6192 9.49163C17.1269 9.03601 17.1269 8.29732 16.6192 7.84171Z"
+                fill="#CDCDD4"
+              />
+            </svg>
+          </div>
         </div>
         <div className={`${styles.sign} ${styles.dynamic}`}>
           <h5 className={styles.name}>Динамика расходов</h5>
           <div className={styles.columns}>
-            {/* {days.map((d, index) => (
-              <div className={styles.column} key={index}>
-                {d}
-              </div>
-            ))} */}
+            {days.map((d, index) => (
+              <div
+                className={styles.column}
+                key={index}
+                style={{ height: `${Math.round(Math.random() * 100)}%` }}
+              ></div>
+            ))}
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles.average}>
+              <h5>Средние траты в день</h5>
+              <h3 className={styles.check}>
+                <span className={styles.value}>{formatNumber(getExpensesValue())} </span>
+                <span className={styles.currency}>₽</span>
+              </h3>
+            </div>
+            <div className={styles.comparison}>
+              <h5>В сравнении с прошлым мес.</h5>
+              <h3 className={styles.check}>
+                <span className={styles.value}>+20 </span>
+                <span className={styles.percent}>%</span>
+              </h3>
+            </div>
           </div>
         </div>
       </div>
