1af32b9e2d710d7b34a1846ac3e1881d973e538a
lorgik <sib.lorgik@gmail.com>
2024-08-28 10:16:56 +0300

    try to fix

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -4,9 +4,24 @@ const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches
 const defaultTheme = isDarkTheme ? 'dark' : 'light'
 
 const themeKey = 'app-theme'
+let getTheme
+
+if (typeof window !== 'undefined') {
+  getTheme = localStorage.getItem(themeKey)
+} else {
+  getTheme = 'light'
+}
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState(localStorage.getItem(themeKey) || defaultTheme)
+  const [theme, setTheme] = useState(getTheme || defaultTheme)
+
+  // useLayoutEffect(() => {
+  //   const theme = localStorage.getItem(themeKey)
+
+  //   if (theme) {
+  //     setTheme(theme)
+  //   }
+  // }, [])
 
   useLayoutEffect(() => {
     document.documentElement.setAttribute('data-theme', theme)
